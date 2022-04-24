df39445ad2187ebb40af543fb38e26e2bc9243fa
nour-s <n.sabouny@gmail.com>
2022-04-24 18:02:28 +0400

    fraudant activity notifications

--- fraudulent-activity-notifications.js
+++ fraudulent-activity-notifications.js
@@ -1,16 +1,15 @@
-function activityNotifications(expenditure, d) {
+function activityNotifications(expenditure, days) {
     let countingSort = new Array(201).fill(0);
-    const medianPos = d % 2 === 0 ? (d / 2) : Math.floor(d / 2) + 1;
+    const medianPos = days % 2 === 0 ? (days / 2) : Math.floor(days / 2) + 1;
     let notifications = 0;
-    // let end = d;
     const len = expenditure.length;
 
-    for (let i = 0; i < d; i++) {
+    for (let i = 0; i < days; i++) {
         countingSort[expenditure[i]]++;
     }
 
-    for (let end = d, current = 0; end < len; end++, current++) {
-        const median = getMedian(countingSort, d, medianPos);
+    for (let end = days, current = 0; end < len; end++, current++) {
+        const median = getMedian(countingSort, days, medianPos);
         if (expenditure[end] >= 2 * median) {
             notifications++;
         }
@@ -24,7 +23,11 @@ function activityNotifications(expenditure, d) {
 
 function getMedian(countingSort, d, medianPos) {
     let left = 0, counter = 0;
+    // Remember, the index of the counterList is the actual value in the expenditure array
+    // So left and right are the actual values
 
+    // Increase the counter by the number of times the value is in the countingSort array
+    // Until we reach the median position or cross it.
     while (counter < medianPos) {
         counter += countingSort[left];
         left++;
@@ -33,7 +36,7 @@ function getMedian(countingSort, d, medianPos) {
     let right = left;
     left--;
 
-    if (counter > medianPos || d % 2 === 0) {
+    if (counter > medianPos || d % 2 !== 0) {
         return left;
     }
     else {
@@ -44,5 +47,5 @@ function getMedian(countingSort, d, medianPos) {
     }
 }
 
-const r = activityNotifications([2, 3, 4, 2, 3, 6, 8, 4, 5], 4);
+const r = activityNotifications([0, 1, 5, 6, 7, 8, 9, 20], 4);
 console.log(r);
