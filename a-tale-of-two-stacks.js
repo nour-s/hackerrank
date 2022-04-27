f9119601e476d159e713e1c8ef9e9b39adf85022
nour-s <n.sabouny@gmail.com>
2022-04-27 16:39:28 +0400

    a tale of two stacks to have input from stdin

--- a-tale-of-two-stacks.js
+++ a-tale-of-two-stacks.js
@@ -2,34 +2,91 @@ class Stack {
     constructor() {
         this.stack = [];
     }
+
     push(item) {
         this.stack.push(item);
     }
     pop() {
-        this.stack.pop();
+        return this.stack.pop();
     }
     peek() {
         return this.stack[this.stack.length - 1];
     }
+
+    get length() { return this.stack.length; }
 }
 
-
-function processData(input) {
+function processData(queries) {
     //Enter your code here
-    const stack1 = new Stack();
-    const stack2 = new Stack();
+    const stackPopPeek = new Stack();
+    const stackPush = new Stack();
+    const result = [];
 
-    const [query, value] = input;
+    for (let i = 0; i < queries.length; i++) {
+        const [query, value] = queries[i];
 
-    switch (input) {
-        case '1':
-            stack1.push(value);
-            break;
-        case '2':
-            stack2.push(value);
-            break;
-        case '3':
-
+        switch (query) {
+            case 1:
+                while (stackPopPeek.length > 0) {
+                    stackPush.push(stackPush.pop());
+                }
+                stackPush.push(value);
+                break;
+            case 2:
+                while (stackPush.length > 0) {
+                    stackPopPeek.push(stackPush.pop());
+                }
+                stackPopPeek.pop();
+                break;
+            case 3:
+                while (stackPush.length > 0) {
+                    stackPopPeek.push(stackPush.pop());
+                }
+                const item = stackPopPeek.peek();
+                result.push(item);
+                break;
+        }
     }
 
-} 
+    return result;
+}
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function (inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function () {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+function main() {
+    const q = parseInt(readLine().trim(), 10);
+
+    let queries = Array(q);
+
+    for (let i = 0; i < q; i++) {
+        queries[i] = readLine().replace(/\s+$/g, '').split(' ').map(queriesTemp => parseInt(queriesTemp, 10));
+    }
+
+    const ans = processData(queries);
+    console.log("ans", ans);
+
+    for (let a of ans) {
+        console.log(a);
+    }
+}
